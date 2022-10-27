d0d891b5c60ddb51211ccc31f52b142c995975c6
dxia2 <d.xia2@share.epsb.ca>
2022-10-27 09:29:07 -0600

    added stuff

--- renderer.js
+++ renderer.js
@@ -23,13 +23,32 @@ class RectRenderer extends Renderer{
         this.height = height;
         this.degrees = degrees;
     }
-    draw(){
+    draw(ctx){
+        // first save the untranslated/unrotated context
+        ctx.save();
 
+        ctx.beginPath();
+        // move the rotation point to the center of the rect
+        ctx.translate(this.pos.x+this.width/2, this.pos.y+this.height/2 );
+        // rotate the rect
+        ctx.rotate(this.degrees*Math.PI/180);
+
+        // draw the rect on the transformed context
+        // Note: after transforming [0,0] is visually [x,y]
+        //       so the rect needs to be offset accordingly when drawn
+        ctx.rect( -this.width/2, -this.height/2, this.width, this.height);
+
+        ctx.fillStyle="green";
+        ctx.fill();
+
+        // restore the context to its untranslated/unrotated state
+        ctx.restore();
     }
 }
 
-function drawAll() {
+function drawAll(ctx) {
+    ctx.clearRect(0, 0, 854, 480);
     for(let i = 0; i < drawList.length; i++){
-        drawList[i].draw();
+        drawList[i].draw(ctx);
     } 
 }
