e7a5ee4dbc02ed894db957dbe4d5dc39e86a9323
destroyer8898 <a.zhang17@share.epsb.ca>
2022-11-02 09:33:53 -0600

    Fix 1

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 let canvas = document.getElementById("canv");
-let hehehaw = document.getElementById("goldTest");
-hehehaw.addEventListener("click", goldLevelIncrease)
 
 let ctx = canvas.getContext("2d");
 
 canvas.width = "854";
 canvas.height = "480";
-class Vector2 {
+class Vector2{
     x = 0;
     y = 0;
-    constructor(x, y) {
+    constructor(x, y){
         this.x = x;
         this.y = y;
     }
@@ -22,35 +20,21 @@ let square = new RectRenderer(new Vector2(0, 0), BACKGROUNDSIZE, BACKGROUNDSIZE,
 let backgroundSquares = [];
 const BACKGROUNDSQUARESSIZE = 30;
 let offsetRow = false;
-for (let y = (BACKGROUNDSIZE / 2); y > -(BACKGROUNDSIZE / 2); y -= BACKGROUNDSQUARESSIZE) {
-    if (!offsetRow) {
-        for (let x = BACKGROUNDSIZE / -2; x < BACKGROUNDSIZE / 2; x += BACKGROUNDSQUARESSIZE * 2) {
-            backgroundSquares.push(new RectRenderer(new Vector2(x, y),
-                BACKGROUNDSQUARESSIZE, BACKGROUNDSQUARESSIZE, "#78D03B", cam));
+for(let y = (BACKGROUNDSIZE / 2); y > -(BACKGROUNDSIZE / 2); y -= BACKGROUNDSQUARESSIZE){
+    if(!offsetRow){
+        for(let x = BACKGROUNDSIZE / -2; x < BACKGROUNDSIZE / 2; x += BACKGROUNDSQUARESSIZE * 2){
+            backgroundSquares.push(new RectRenderer(new Vector2(x, y), 
+            BACKGROUNDSQUARESSIZE, BACKGROUNDSQUARESSIZE, "#78D03B", cam));
         }
         offsetRow = true;
-    } else {
-        for (let x = (BACKGROUNDSIZE / -2) + BACKGROUNDSQUARESSIZE; x < BACKGROUNDSIZE / 2; x += BACKGROUNDSQUARESSIZE * 2) {
-            backgroundSquares.push(new RectRenderer(new Vector2(x, y),
-                BACKGROUNDSQUARESSIZE, BACKGROUNDSQUARESSIZE, "#78D03B", cam));
+    }else{
+        for(let x = (BACKGROUNDSIZE / -2) + BACKGROUNDSQUARESSIZE; x < BACKGROUNDSIZE / 2; x += BACKGROUNDSQUARESSIZE * 2){
+            backgroundSquares.push(new RectRenderer(new Vector2(x, y), 
+            BACKGROUNDSQUARESSIZE, BACKGROUNDSQUARESSIZE, "#78D03B", cam));
         }
         offsetRow = false
     }
 
-}
-
-// Mouse movement (snap mouse to grid)
-document.addEventListener('mousemove', (event) => {
-    let mousePos = getMousePos(canvas, event);
-    console.log(mousePos);
-});
-
-function getMousePos(canvas, evt) {
-    var rect = canvas.getBoundingClientRect();
-    return {
-        x: evt.clientX - rect.left,
-        y: evt.clientY - rect.top
-    };
 }
 
 // Camera Movement
@@ -71,25 +55,18 @@ function keydownHandler(event) {
     }
 }
 
+function keyupHandler(event) {
+    if (event.code == "KeyW" || event.code == "KeyS") {
+        upDownValue = 0;
+    } else if (event.code == "KeyA" || event.code == "KeyD") {
+        leftRightValue = 0;
+    }
+}
+
 function loop(time){
-  cam.pos.y += upDownValue;
+    cam.pos.y += upDownValue;
     cam.pos.x += leftRightValue;
     drawAll(ctx);
     requestAnimationFrame(loop);
 }
 requestAnimationFrame(loop);
-
-
-let gold = 0;
-function goldLevelIncrease() {
-    let goldLevel = 1
-    goldLevel + 1;
-    console.log(goldLevel)
-    
-window.setInterval(
-function constantGoldIncrease() {
-    gold = gold + goldLevel;
-    console.log(gold);
-},
-600);
-}
