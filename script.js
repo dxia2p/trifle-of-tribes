07bf8b902d97159bec9378d1da640a24d27b76d4
MilkBefourCereal <112642195+MilkBefourCereal@users.noreply.github.com>
2022-11-02 09:27:56 -0600

    Added gold counter

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let canvas = document.getElementById("canv");
+let hehehaw = document.getElementById("goldTest");
+hehehaw.addEventListener("click", goldLevelIncrease)
 
 let ctx = canvas.getContext("2d");
 
@@ -18,8 +20,23 @@ let cam = new Camera(854, 480, new Vector2(0, 0));
 let square = new RectRenderer(new Vector2(0, 0), 30, 30, 0, "green", cam);
 
 function loop(time){
-    
+  
     drawAll(ctx);
     requestAnimationFrame(loop);
 }
 requestAnimationFrame(loop);
+
+
+let gold = 0;
+function goldLevelIncrease() {
+    let goldLevel = 1
+    goldLevel + 1;
+    console.log(goldLevel)
+    
+window.setInterval(
+function constantGoldIncrease() {
+    gold = gold + goldLevel;
+    console.log(gold);
+},
+600);
+}
